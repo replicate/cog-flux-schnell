a2e8b65ee0ee0570b0dbd99127f362273661e45e
lucataco <me@lucataco.com>
2024-08-04 03:56:25 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# black-forest-labs/FLUX.1-schnell Cog Model
+
+This is an implementation of [black-forest-labs/FLUX.1-schnell](https://huggingface.co/black-forest-labs/FLUX.1-schnell) as a [Cog](https://github.com/replicate/cog) model.
+
+## Development
+
+Follow the [model pushing guide](https://replicate.com/docs/guides/push-a-model) to push your own model to [Replicate](https://replicate.com).
+
+
+## How to use
+
+Make sure you have [cog](https://github.com/replicate/cog) installed.
+
+To run a prediction:
+
+    cog predict -i prompt="a tiny astronaut hatching from an egg on the moon"
+
+![Output](output.png)
---
+++ predict.py
@@ -0,0 +1,159 @@
+# Prediction interface for Cog ⚙️
+# https://github.com/replicate/cog/blob/main/docs/python.md
+
+from cog import BasePredictor, Input, Path
+import os
+import time
+import torch
+import subprocess
+import numpy as np
+from PIL import Image
+from typing import List
+from diffusers import FluxPipeline
+from transformers import CLIPImageProcessor
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker
+)
+
+MODEL_CACHE = "checkpoints"
+MODEL_URL = "https://weights.replicate.delivery/default/black-forest-labs/FLUX.1-schnell/model.tar"
+SAFETY_CACHE = "safety-cache"
+FEATURE_EXTRACTOR = "/src/feature-extractor"
+SAFETY_URL = "https://weights.replicate.delivery/default/sdxl/safety-1.0.tar"
+
+def download_weights(url, dest):
+    start = time.time()
+    print("downloading url: ", url)
+    print("downloading to: ", dest)
+    subprocess.check_call(["pget", "-x", url, dest], close_fds=False)
+    print("downloading took: ", time.time() - start)
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        start = time.time()
+
+        print("Loading safety checker...")
+        if not os.path.exists(SAFETY_CACHE):
+            download_weights(SAFETY_URL, SAFETY_CACHE)
+        self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
+            SAFETY_CACHE, torch_dtype=torch.float16
+        ).to("cuda")
+        self.feature_extractor = CLIPImageProcessor.from_pretrained(FEATURE_EXTRACTOR)
+        
+        print("Loading Flux txt2img Pipeline")
+        if not os.path.exists(MODEL_CACHE):
+            download_weights(MODEL_URL, MODEL_CACHE)
+        self.txt2img_pipe = FluxPipeline.from_pretrained(
+            MODEL_CACHE,
+            torch_dtype=torch.bfloat16
+        ).to("cuda")
+
+        # Save some VRAM by offloading the model to CPU
+        vram = int(torch.cuda.get_device_properties(0).total_memory/(1024*1024*1024))
+        if vram < 50:
+            print("GPU VRAM < 50Gb - Offloading model to CPU")
+            self.txt2img_pipe.enable_model_cpu_offload()
+        
+        print("setup took: ", time.time() - start)
+
+    @torch.cuda.amp.autocast()
+    def run_safety_checker(self, image):
+        safety_checker_input = self.feature_extractor(image, return_tensors="pt").to("cuda")
+        np_image = [np.array(val) for val in image]
+        image, has_nsfw_concept = self.safety_checker(
+            images=np_image,
+            clip_input=safety_checker_input.pixel_values.to(torch.float16),
+        )
+        return image, has_nsfw_concept
+
+    def aspect_ratio_to_width_height(self, aspect_ratio: str):
+        aspect_ratios = {
+            "1:1": (1024, 1024),"16:9": (1344, 768),"21:9": (1536, 640),
+            "3:2": (1216, 832),"2:3": (832, 1216),"4:5": (896, 1088),
+            "5:4": (1088, 896),"9:16": (768, 1344),"9:21": (640, 1536),
+        }
+        return aspect_ratios.get(aspect_ratio)
+
+    @torch.inference_mode()
+    def predict(
+        self,
+        prompt: str = Input(description="Prompt for generated image"),
+        aspect_ratio: str = Input(
+            description="Aspect ratio for the generated image",
+            choices=["1:1", "16:9", "21:9", "2:3", "3:2", "4:5", "5:4", "9:16", "9:21"],
+            default="1:1"),
+        num_outputs: int = Input(
+            description="Number of images to output.",
+            ge=1,
+            le=4,
+            default=1,
+        ),
+        seed: int = Input(description="Random seed. Set for reproducible generation", default=None),
+        output_format: str = Input(
+            description="Format of the output images",
+            choices=["webp", "jpg", "png"],
+            default="webp",
+        ),
+        output_quality: int = Input(
+            description="Quality when saving the output images, from 0 to 100. 100 is best quality, 0 is lowest quality. Not relevant for .png outputs",
+            default=80,
+            ge=0,
+            le=100,
+        ),
+        disable_safety_checker: bool = Input(
+            description="Disable safety checker for generated images. This feature is only available through the API. See [https://replicate.com/docs/how-does-replicate-work#safety](https://replicate.com/docs/how-does-replicate-work#safety)",
+            default=False,
+        ),
+    ) -> List[Path]:
+        """Run a single prediction on the model"""
+        if seed is None:
+            seed = int.from_bytes(os.urandom(2), "big")
+        print(f"Using seed: {seed}")
+
+        width, height = self.aspect_ratio_to_width_height(aspect_ratio)
+
+        guidance_scale=0.0
+        num_inference_steps=4
+        max_sequence_length=256
+
+        flux_kwargs = {}
+        print(f"Prompt: {prompt}")
+        print("txt2img mode")
+        flux_kwargs["width"] = width
+        flux_kwargs["height"] = height
+        pipe = self.txt2img_pipe
+
+        generator = torch.Generator("cuda").manual_seed(seed)
+
+        common_args = {
+            "prompt": [prompt] * num_outputs,
+            "guidance_scale": guidance_scale,
+            "generator": generator,
+            "num_inference_steps": num_inference_steps,
+            "max_sequence_length": max_sequence_length,
+            "output_type": "pil"
+        }
+
+        output = pipe(**common_args, **flux_kwargs)
+
+        if not disable_safety_checker:
+            _, has_nsfw_content = self.run_safety_checker(output.images)
+
+        output_paths = []
+        for i, image in enumerate(output.images):
+            if not disable_safety_checker and has_nsfw_content[i]:
+                print(f"NSFW content detected in image {i}")
+                continue
+            output_path = f"/tmp/out-{i}.{output_format}"
+            if output_format != 'png':
+                image.save(output_path, quality=output_quality, optimize=True)
+            else:
+                image.save(output_path)
+            output_paths.append(Path(output_path))
+
+        if len(output_paths) == 0:
+            raise Exception("NSFW content detected. Try running it again, or try a different prompt.")
+
+        return output_paths
+    
